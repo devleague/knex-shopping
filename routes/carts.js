ecdadbec7a0331251d9517347e773763a5b40588
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-10-03 21:26:20 -1000

    supports DELETE from carts/:user_id/:products_id

--- routes/carts.js
+++ routes/carts.js
@@ -3,7 +3,15 @@ const db = require("../database");
 
 const router = express.Router();
 
-router.get("/:user_id", (req, res) => {});
+router.get("/:user_id", (req, res) => {
+  //   db.raw("SELECT * FROM carts", req.params.user_id)
+  //     .then(results => {
+  //       res.json(results.rows);
+  //     })
+  //     .catch(err => {
+  //       res.status(500).json({ message: err.message });
+  //     });
+});
 
 router.post("/:user_id/:products_id", (req, res) => {
   db.raw("INSERT INTO carts (user_id, products_id) VALUES (?, ?) RETURNING *", [
@@ -18,6 +26,17 @@ router.post("/:user_id/:products_id", (req, res) => {
     });
 });
 
-router.delete("/:user_id/:products_id", (req, res) => {});
+router.delete("/:user_id/:products_id", (req, res) => {
+  db.raw("DELETE FROM carts WHERE user_id = ? AND products_id = ?", [
+    req.params.user_id,
+    req.params.products_id
+  ])
+    .then(results => {
+      res.json({ success: true });
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
 
 module.exports = router;
