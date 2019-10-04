c99d803735d711dc0e680313bf75ee1b35d5349f
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-10-03 20:37:40 -1000

    supports PUT and DELETE to products/

--- routes/products.js
+++ routes/products.js
@@ -58,4 +58,64 @@ router.post("/new", (req, res) => {
     });
 });
 
+router.put("/:product_id", (req, res) => {
+  db.raw("SELECT * FROM products WHERE id = ?", req.params.product_id)
+    .then(results => {
+      if (results.rows.length === 0) {
+        res
+          .status(500)
+          .json({ message: `Product ${req.params.product_id} not found` });
+      } else if (
+        !req.body.title &&
+        !req.body.description &&
+        !req.body.inventory &&
+        !req.body.price
+      ) {
+        res
+          .status(500)
+          .json({ message: "Must PUT at least one product field" });
+      } else {
+        db.raw(
+          "UPDATE products SET title = ?, description = ?, inventory = ?, price = ? WHERE id = ? RETURNING *",
+          [
+            req.body.title,
+            req.body.description,
+            req.body.inventory,
+            req.body.price,
+            req.params.product_id
+          ]
+        ).then(results => {
+          res.json({
+            message: `Product: ${results.rows[0].id} has been updated`
+          });
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.delete("/:product_id", (req, res) => {
+  db.raw("SELECT * FROM products WHERE id = ?", [req.params.product_id])
+    .then(results => {
+      if (results.rows.length === 0) {
+        res
+          .status(500)
+          .json({ message: `Product ${req.params.product_id} not found` });
+      } else {
+        db.raw("DELETE FROM products WHERE id = ? RETURNING *", [
+          req.params.product_id
+        ]).then(results => {
+          res.json({
+            message: `Product id: ${results.rows[0].id} successfully deleted`
+          });
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
 module.exports = router;
