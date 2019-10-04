ceec164a64c018da4cfc4bd62849ed20fb832fcd
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-10-03 21:17:26 -1000

    supports POST to carts/:user_id/:products_id

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,21 @@ const db = require("../database");
 
 const router = express.Router();
 
-router.get("/carts", (req, res) => {});
+router.get("/:user_id", (req, res) => {});
+
+router.post("/:user_id/:products_id", (req, res) => {
+  db.raw("INSERT INTO carts (user_id, products_id) VALUES (?, ?) RETURNING *", [
+    req.params.user_id,
+    req.params.products_id
+  ])
+    .then(results => {
+      res.json({ success: true });
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.delete("/:user_id/:products_id", (req, res) => {});
 
 module.exports = router;
