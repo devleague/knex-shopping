de9cad62d21afc1dba089bb0aa0891ca7fd01024
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-10-03 21:34:18 -1000

    supports GET from carts/:user_id

--- routes/carts.js
+++ routes/carts.js
@@ -4,13 +4,16 @@ const db = require("../database");
 const router = express.Router();
 
 router.get("/:user_id", (req, res) => {
-  //   db.raw("SELECT * FROM carts", req.params.user_id)
-  //     .then(results => {
-  //       res.json(results.rows);
-  //     })
-  //     .catch(err => {
-  //       res.status(500).json({ message: err.message });
-  //     });
+  db.raw(
+    "SELECT products.id, products.title, products.description, products.inventory, products.price FROM products LEFT JOIN carts ON products.id = carts.products_id LEFT JOIN users ON carts.user_id = users.id WHERE users.id = ?",
+    req.params.user_id
+  )
+    .then(results => {
+      res.json(results.rows);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
 });
 
 router.post("/:user_id/:products_id", (req, res) => {
