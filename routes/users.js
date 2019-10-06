e6520382a4f4f85078bdddaf194973d6b1564de9
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-10-05 17:05:58 -1000

    replaces raw SQL with query builder syntax in routes/users.js/

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ const db = require("../database");
 const router = express.Router();
 
 router.get("/:user_id", (req, res) => {
-  db.raw("SELECT * FROM users WHERE id = ?", req.params.user_id)
+  db("users")
+    .where("id", req.params.user_id)
     .then(results => {
-      if (results.rows.length === 0) {
+      if (results.length === 0) {
         res.status(500).json({ message: "User not found" });
       } else {
-        res.json(results.rows);
+        res.json(results[0]);
       }
     })
     .catch(err => {
@@ -18,9 +19,9 @@ router.get("/:user_id", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  db.raw("SELECT * FROM users")
+  db("users")
     .then(results => {
-      res.json(results.rows);
+      res.json(results);
     })
     .catch(err => {
       res.status(500).json({ message: err.message });
@@ -28,13 +29,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  db.raw("SELECT * FROM users WHERE email = ?", req.body.email)
+  db("users")
+    .where("email", req.body.email)
     .then(results => {
-      if (results.rows.length === 0) {
+      if (results.length === 0) {
         res.status(500).json({ message: "User not found" });
-      } else if (results.rows[0].password !== req.body.password) {
+      } else if (results[0].password !== req.body.password) {
         res.status(500).json({ message: "Incorrect password" });
-      } else res.json(results.rows);
+      } else res.json(results[0]);
     })
     .catch(err => {
       res.status(500).json({ message: err.message });
@@ -42,19 +44,19 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  db.raw("SELECT email FROM users WHERE email = ?", req.body.email)
+  db("users")
+    .where("email", req.body.email)
     .then(results => {
-      if (results.rows.length > 0) {
+      if (results.length > 0) {
         res.status(500).json({ message: "User already exists" });
       } else if (!req.body.email || !req.body.password) {
         res.status(500).json({ message: "Must POST email and password" });
       } else {
-        db.raw(
-          "INSERT INTO users (email, password) VALUES (?, ?) RETURNING *",
-          [req.body.email, req.body.password]
-        ).then(results => {
-          res.json(results.rows);
-        });
+        db("users")
+          .insert({ email: req.body.email, password: req.body.password }, "*")
+          .then(results => {
+            res.json(results[0]);
+          });
       }
     })
     .catch(err => {
@@ -63,12 +65,23 @@ router.post("/register", (req, res) => {
 });
 
 router.put("/:user_id/forgot-password", (req, res) => {
-  db.raw(
-    "UPDATE users SET password = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ? RETURNING *",
-    [req.body.password, req.params.user_id]
-  )
+  db("users")
+    .where("id", req.params.user_id)
     .then(results => {
-      res.json({ message: "New password created!" });
+      if (results.length === 0) {
+        res.status(500).json({ message: "User not found" });
+      } else {
+        let date = new Date().toISOString();
+        db("users")
+          .where({ id: req.params.user_id })
+          .update({ password: req.body.password, updated_at: date })
+          .then(results => {
+            res.json({ message: "New password created!" });
+          })
+          .catch(err => {
+            res.status(500).json({ message: err.message });
+          });
+      }
     })
     .catch(err => {
       res.status(500).json({ message: err.message });
@@ -76,18 +89,21 @@ router.put("/:user_id/forgot-password", (req, res) => {
 });
 
 router.delete("/:user_id", (req, res) => {
-  db.raw("SELECT * FROM users WHERE id = ?", [req.params.user_id])
+  db("users")
+    .where({ id: req.params.user_id })
     .then(results => {
-      if (results.rows.length === 0) {
+      if (results.length === 0) {
         res.status(500).json({ message: "User ID not found" });
       } else {
-        db.raw("DELETE FROM users WHERE id = ? RETURNING *", [
-          req.params.user_id
-        ]).then(results => {
-          res.json({
-            message: `User id: ${results.rows[0].id} successfully deleted`
+        db("users")
+          .where({ id: req.params.user_id }, "*")
+          .del()
+          .returning("*")
+          .then(results => {
+            res.json({
+              message: `User id: ${results[0].id} successfully deleted`
+            });
           });
-        });
       }
     })
     .catch(err => {
